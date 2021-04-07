1c29ea6f3d70ee583be27141b516aaff243bfc52
Jamie H Pham <jpham1109@gmail.com>
2021-04-07 18:02:58 -0400

    First bonus done

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 
 const Comment = ({ user, comment }) => {
+
+    const [upvote, setUpvotes] = useState(0)
+    const [downvote, setDownvotes] = useState(0)
+
+    function handleUpvote() {
+        setUpvotes((upvote) => upvote + 1)
+    }
+    function handleDownvote() {
+        setDownvotes((downvote) => downvote + 1)
+    }
     return (
         <div className="user-comment">
             <h4>{user}</h4>
             <p>{comment}</p>
+            <button onClick={handleUpvote} className="upvotes">{upvote}👍</button> <button onClick={handleDownvote} className="downvotes">{downvote}👎</button>
         </div>
     )
 }
+
 
 export default Comment;
