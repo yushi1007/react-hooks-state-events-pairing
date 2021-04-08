c8544002b3eacbfb197273e0bf1feb0cfa340197
Jamie H Pham <jpham1109@gmail.com>
2021-04-07 21:00:29 -0400

    2nd bonus done, 2 bnonuses left

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,22 +1,43 @@
 import Comment from "./Comment";
 import React, {useState} from "react";
 
-const Comments = (comments) => {
-    // debugger
-    const commentArray = comments.comments
-    const commentItems = commentArray.map((comment) => {
+const Comments = ({comments}) => {
+
+    const [searchText, setSearchText] = useState('')
+
+    const filteredComments = comments.filter((comment) => {
+        return comment.user.includes(searchText);
+    })
+
+    const [currentState, setState] = useState('none')
+    function handleCommentsToggle () {
+        setState((currentState) => !currentState)
+    }
+
+    const commentItems = filteredComments.map((comment) => {
         return (
             <Comment 
             key={comment.id}
+            id={comment.id}
             user={comment.user}
             comment={comment.comment}
+            onDelete={handleDelete}
             />
         )
-    }) 
-    const [currentState, setState] = useState('none')
+    })
 
-    function handleCommentsToggle () {
-        setState((currentState) => !currentState)
+    const [commentList, setCommentList] = useState(commentItems)
+    function handleDelete (id) {
+        console.log(id)
+        const newComments = commentList.filter((comment) => { 
+            console.log(comment)
+            return comment.id !== id})
+        console.log(newComments)
+        setCommentList(newComments)
+    }
+    console.log(commentList)
+    function handleSearch (event) {
+        setSearchText(event.target.value) 
     }
 
     return(
@@ -24,8 +45,10 @@ const Comments = (comments) => {
         <div className="button">
             <button className="comments-toggle" onClick={handleCommentsToggle}>{currentState ? "Hide" : "Show" } Comments</button>
         </div>
+        <input onChange={handleSearch} type="text" placeholder="Search" />
+
         <div className="comments" style={{ display: currentState ? "block" : "none" }}>
-            <h3>{commentArray.length} Comments</h3>
+            <h3>{comments.length} Comments</h3>
             {commentItems}
         </div>
         </>
